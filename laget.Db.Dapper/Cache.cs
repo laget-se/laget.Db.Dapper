ee04dac7fe36d151c57cdd2a5c71d242b31750a0
Erik Hennerfors <erik.hennerfors@obscured.se>
2020-12-09 09:54:36 +0100

    Added support for cached repository

---
+++ laget.Db.Dapper/Cache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace laget.Db.Dapper
+{
+    public class Cache
+    {
+        [Required]
+        public string KeyPrefix { get; set; }
+        public TimeSpan Expiration { get; set; } = TimeSpan.FromMinutes(15);
+
+        public Cache(string keyPrefix)
+        {
+            KeyPrefix = keyPrefix;
+        }
+    }
+}
