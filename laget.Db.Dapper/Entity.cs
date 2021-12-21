0e642e1e229dd5d026bbe5cfd42ae415a28c3ca6
Erik Hennerfors <erik.hennerfors@laget.se>
2021-12-21 10:27:41 +0100

    Added virtual to CreatedAt and UpdatedAt so to make it overridable

--- laget.Db.Dapper/Entity.cs
+++ laget.Db.Dapper/Entity.cs
@@ -5,8 +5,8 @@ namespace laget.Db.Dapper
     public abstract class Entity
     {
         public virtual int Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public virtual DateTime CreatedAt { get; set; }
+        public virtual DateTime UpdatedAt { get; set; }
 
         public abstract object ToObject();
     }
